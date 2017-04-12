5b217c9c7cf09d4cacb86516239d154b46515b7c
mulhauser <mulhauser.remy@gmail.comr>
2017-04-12 11:46:16 +0200

    Map

--- BatailleNavale/src/bataillenavale/Map.java
+++ BatailleNavale/src/bataillenavale/Map.java
@@ -4,4 +4,12 @@ package bataillenavale;
  * Created by mulhauser on 12/04/2017.
  */
 public class Map {
+
+    private int[][] map;
+    private int size;
+
+    public Map(int s){
+        this.size = s;
+        this.map = new int[this.size][this.size];
+    }
 }
--- BatailleNavale/src/bataillenavale/Player.java
+++ BatailleNavale/src/bataillenavale/Player.java
@@ -1,6 +1,8 @@
 package bataillenavale;
 
 
+import java.util.ArrayList;
+
 /**
  * Created by mulhauser on 12/04/2017.
  */
@@ -8,13 +10,16 @@ public abstract class Player {
 
     private String pseudo;
     private int score;
+    private Map map;
+    private ArrayList<Ship> fleet;
 
-    public Player(String p){
+    public Player(String p, int size){
         this.pseudo = p;
         this.score = 0;
+        this.map = new Map(size);
+        this.fleet = new ArrayList<Ship>(4);
     }
 
-    public void putShip(Ship s, Map m){
-
-    }
+    public abstract void putShip(Ship s)
+    public abstract void fire();
 }
