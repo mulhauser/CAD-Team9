2546d7ee3a490bc1ab265822f6b97c147134b6b0
wolkowicz <michel.wolkowicz7@etu.univ-lorraine.fr>
2017-04-19 16:52:32 +0200

    placementbato

--- BatailleNavale/src/bataillenavale/player/Map.java
+++ BatailleNavale/src/bataillenavale/player/Map.java
@@ -42,13 +42,88 @@ public class Map {
 
     /**
      * va ajouter un bateau dans le tableau disposition
+     * appèle une fonction qui vérifie puis une qui place
      * @param s
      */
-    public void ajouterBateau(Ship s, int x, int y){
+    public void ajouterBateau(Ship s, int x, int y, Ship.Orientation orientation){
         if(s.getSize() == 2){
-
+            // verificationDeplacement retourne true si tout est bien, sinon false
+            if(verificationsDepacement(x, y, s.getSize(), orientation)){
+               // mapDispositionBateaux[x][y]
+            }
         }
     }
+
+
+
+    /**
+     * fonction qui vérifie si le bateau peut être placé à l'endoit séléctionné
+     * @param x
+     * @param y
+     * @param size
+     * @param orientation
+     * @return
+     */
+    public boolean verificationsDepacement(int x, int y, int size, Ship.Orientation orientation){
+        boolean result = false;
+        switch (orientation){
+            case TOP:
+                if(y + size > mapDispositionBateaux.length){
+                    result = false;
+                }else{
+                    result = true;
+                }
+                break;
+            case BOTTOM:
+                if(y - size < mapDispositionBateaux.length){
+                    result = false;
+                }else{
+                    result = true;
+                }
+                break;
+            case LEFT:
+                if(x - size < mapDispositionBateaux.length){
+                    result = false;
+                }else{
+                    result = true;
+                }
+                break;
+            case RIGHT:
+                if(x + size < mapDispositionBateaux.length){
+                    result = false;
+                }else{
+                    result = true;
+                }
+                break;
+        }
+
+        return true;
+    }
+
+
+
+
+    public void placementBateau(int x, int y, int size, Ship.Orientation orientation){
+        // ajouter un switch sur la taille
+        switch (orientation){
+            case TOP:
+
+                break;
+            case BOTTOM:
+
+                break;
+            case LEFT:
+
+                break;
+            case RIGHT:
+                for(int i = x; i < x + size; i++){
+                    mapDispositionBateaux[i][y] = 3;
+                }
+                break;
+        }
+    }
+
+
 
 
     public int[][] getMapDispositionBateaux() {
